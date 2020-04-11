c53f15a08a6e31fd8822acd42ce12ded2f5a70ff
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-04-11 17:17:12 +0530

    req added

--- http.js
+++ http.js
@@ -1,8 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
-    res.write('Hello there user');
-    res.end();
+    if (req.url === '/') {
+        res.write('<h1>Welcome to the homepage</h1>');
+        res.end();
+    }
+    if (req.url === '/user') {
+        res.write('Welcome user jhonson');
+        res.end();
+    }
 });
 
 server.listen(3001, () => console.log('Server is up and running'));
