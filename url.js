393856237adf33db5f146637d15500bcfa6f1461
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-04-11 16:40:53 +0530

    print fileExt

---
+++ url.js
@@ -0,0 +1,7 @@
+const url = require('url');
+
+const adress = 'http://localhost:8080/default.html?year=2017&month=february';
+
+const parsedUrl = url.parse(adress, true);
+
+console.log(parsedUrl.host);
