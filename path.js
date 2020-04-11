dd6f9a6211e8e0d4a688b9784822f4545b119e39
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-04-11 16:25:22 +0530

    print file name in path.js

--- path.js
+++ path.js
@@ -2,4 +2,6 @@ const path = require('path');
 
 const fileLocation = path.join(__dirname, 'app.js');
 
-console.log(fileLocation);
+const fileName = path.basename(fileLocation);
+
+console.log(fileName);
