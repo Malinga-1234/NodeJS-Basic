908543f5c965c710621bf7e3e21396a457a4b9a6
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-04-11 17:38:28 +0530

    create package.json

--- http.js
+++ http.js
@@ -1,12 +1,15 @@
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
 const server = http.createServer((req,res) => {
     if (req.url === '/') {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<h1>Welcome to the homepage</h1>');
-        res.write('<h1>Welcome to the homepage</h1>');
-        res.write('<p>Additionalcontent</p>');
-        res.end();
+
+        fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        });
     }
     if (req.url === '/user') {
         res.write('Welcome user jhonson');
