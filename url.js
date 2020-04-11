0bee67162396287f3a2827a2b85e9b10ad28f79b
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-04-11 16:46:11 +0530

    print url pathname and query

--- url.js
+++ url.js
@@ -5,3 +5,6 @@ const adress = 'http://localhost:8080/default.html?year=2017&month=february';
 const parsedUrl = url.parse(adress, true);
 
 console.log(parsedUrl.host);
+console.log(parsedUrl.pathname);
+console.log(parsedUrl.query);
+console.log(parsedUrl.query.month);
