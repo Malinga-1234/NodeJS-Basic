c029f8f64e539301191f3448e128d6f43ca5f72f
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-04-11 16:58:44 +0530

    read  message in txt file

--- fs.js
+++ fs.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
 
-fs.writeFile('message.txt', 'Hello there node', (err) => {
-    if(err) throw err;
+// fs.writeFile('message.txt', 'Hello there node', (err) => {
+//     if(err) throw err;
 
-    console.log('file has been written');
+//     console.log('file has been written');
+// });
+
+fs.readFile('./message.txt', 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
 });
